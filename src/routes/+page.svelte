2875c8729aa5affc5315a9e21f42cd86d3cc01da
shibest <eerkingtheawesome@gmail.com>
2024-01-15 17:39:05 -0800

    get started button
    template, do what you want with this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-	<title>Home - Zen Rhythm</title>
+	<title>home - respir</title>
 	<meta name="description" content="" />
 </svelte:head>
 
@@ -12,6 +12,8 @@
 		<h1>respir <span class="text-icon"><IconAir /></span></h1>
 
 		<p class="description">breathing exercises to help you relax, focus, or sleep.</p>
+		<br />
+		<a class="start-button" href="/breathe" title="Get Started"><span>Get Started</span></a>
 	</div>
 </main>
 
@@ -31,5 +33,44 @@
 	.description {
 		font-size: 1.2rem;
 		margin: 1rem 0;
+	}
+
+	.start-button {
+		padding: 1rem;
+		background-color: hsla(208, 100%, 97%, 0.6);
+		border-radius: 0.5rem;
+		text-decoration: none;
+		color: rgb(18, 29, 27);
+		cursor: pointer;
+		transition: 0.25s;
+	}
+
+	.start-button span {
+		cursor: pointer;
+		transition: 0.5s;
+		margin-right: -0.5rem;
+	}
+
+	.start-button span:after {
+		content: '▶';
+		position: relative;
+		opacity: 0;
+		right: 0.5rem;
+		margin-right: -0.25rem;
+		transition: 0.5s;
+	}
+
+	.start-button:hover span {
+		padding-right: 25px;
+	}
+
+	.start-button:hover span:after {
+		opacity: 1;
+		right: -0.5rem;
+		position: relative;
+	}
+
+	.start-button:hover {
+		background-color: hsla(0, 0%, 100%, 0.8);
 	}
 </style>
