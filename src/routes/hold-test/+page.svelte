0662b7a7a1724a31c5b21bf30d7bcfcde2a5e105
refact0r <yifanw06@gmail.com>
2024-01-15 20:30:55 -0800

    hold test page

---
+++ src/routes/hold-test/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title></title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<main>
+	<h1>breath holding test</h1>
+</main>
+
+<style lang="scss">
+	main {
+		flex: 1;
+		flex-direction: column;
+
+		@include flexCenter;
+	}
+</style>
