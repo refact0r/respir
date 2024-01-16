e5be7e514d73744ea1d5a2c014158679c99ac8ea
ben-6 <ben.yue.hu2@gmail.com>
2024-01-15 19:43:36 -0800

    fix reset bug, timing fix

--- src/routes/BreathePage.svelte
+++ src/routes/BreathePage.svelte
@@ -80,6 +80,7 @@
 		play = false;
 		stopAudio();
 		cycle = -2;
+		step = 0;
 		count = 0;
 		text = 'click play to start';
 		if (exercise.animation === 'box') {
@@ -144,8 +145,8 @@
 						cycle++;
 						// all cycles complete
 						if (cycle === exercise.cycles) {
-							reset();
 							cycle = exercise.cycles;
+							reset();
 							return;
 						}
 					} else {
@@ -213,7 +214,7 @@
 			if (count === 1) {
 				textElem.style.opacity = progress;
 				if (step === exercise.routine.length - 1 && cycle < exercise.cycles) {
-					barElem.style.width = `${(cycle / exercise.cycles) * 100 + (((elapsedSec - 800) / 200) * 100) / exercise.cycles}%`;
+					barElem.style.width = `${Math.min((cycle / exercise.cycles) * 100 + (((elapsedSec - 800) / 200) * 100) / exercise.cycles, ((cycle+1) / exercise.cycles) * 100)}%`;
 				}
 			}
 		} else if (elapsedSec <= 200) {
