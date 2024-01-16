d1f4b605dbb4e2a5baefe4673660e8cace7f6743
shibest <eerkingtheawesome@gmail.com>
2024-01-15 22:10:19 -0800

    more qol changes

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -16,27 +16,28 @@
 
 <main>
 	<h1>settings</h1>
+	<br />
+	<div class="sliders">
+		<label for="ambienceVolume">Ambience Volume:</label>
+		<input
+			type="range"
+			min="0"
+			max="1"
+			step="0.1"
+			bind:value={$ambienceVolume}
+			on:input={handleAmbienceVolumeChange}
+		/>
 
-	<label for="ambienceVolume">Ambience Volume:</label>
-	<input
-		type="range"
-		min="0"
-		max="1"
-		step="0.1"
-		bind:value={$ambienceVolume}
-		on:input={handleAmbienceVolumeChange}
-	/>
-
-	<label for="speechVolume">Speech Volume:</label>
-	<input
-		type="range"
-		min="0"
-		max="1"
-		step="0.1"
-		bind:value={$speechVolume}
-		on:input={handleSpeechVolumeChange}
-	/>
-
+		<label for="speechVolume">Speech Volume:</label>
+		<input
+			type="range"
+			min="0"
+			max="1"
+			step="0.1"
+			bind:value={$speechVolume}
+			on:input={handleSpeechVolumeChange}
+		/>
+	</div>
 	<a href="/" class="icon-button"><IconCheck style="font-size: 1.5rem;" /></a>
 </main>
 
@@ -56,6 +57,12 @@
 	label {
 		display: block;
 		margin-bottom: 10px;
+	}
+
+	.sliders {
+		padding: 2rem;
+		border-radius: 2rem;
+		background-color: var(--bg-2);
 	}
 
 	.icon-button {
