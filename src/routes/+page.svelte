ae2b2e676c2e3a3b9466df6288ad90f008ec18de
refact0r <yifanw06@gmail.com>
2024-01-17 01:37:53 -0800

    add fade transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -74,8 +74,8 @@
 
 <main>
 	{#if showCycles}
-		<div class="form-popup">
-			<form class="form-container">
+		<div class="form-popup" transition:fade={{ duration: 200 }}>
+			<form class="form-container" transition:fade={{ duration: 200 }}>
 				<label for="cycles">number of cycles</label>
 				<br />
 				<input type="number" min="1" step="1" bind:value={currentCycles} />
